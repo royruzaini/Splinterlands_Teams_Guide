bd985bac24bd4327555e4f81235573509d1d7a88
Ruzaini Subri <royruzaini@gmail.com>
2022-02-28 01:28:34 +0800

    secret key

--- Splinterlands_Teams_Guide/settings.py
+++ Splinterlands_Teams_Guide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h^+f)7%keb323+cd)tz*3%!f!lchsc_9f2&8mk^ve8kgp(6(64'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
