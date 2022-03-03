b22484bc78674294284a66a152fc94c82e8429a2
Ruzaini Subri <royruzaini@gmail.com>
2022-03-04 01:49:12 +0800

    website

--- Splinterlands_Teams_Guide/settings.py
+++ Splinterlands_Teams_Guide/settings.py
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
     ]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
