dfda3d702232e65e1ceeeed5f62eee316fd5fc14
Ruzaini Subri <royruzaini@gmail.com>
2022-03-05 20:33:26 +0800

    update readme

--- Splinterlands_Teams_Guide/settings.py
+++ Splinterlands_Teams_Guide/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['splinterlands-teams-guide.herokuapp.com', 'http://127.0.0.1/']
+ALLOWED_HOSTS = ['splinterlands-teams-guide.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
