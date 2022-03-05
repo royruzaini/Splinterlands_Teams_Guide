b0dcabae7bc62ee0eff28d3b2cee578bccfb9260
Ruzaini Subri <royruzaini@gmail.com>
2022-03-05 20:30:09 +0800

    env file

--- Splinterlands_Teams_Guide/settings.py
+++ Splinterlands_Teams_Guide/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['splinterlands-teams-guide.herokuapp.com']
+ALLOWED_HOSTS = ['splinterlands-teams-guide.herokuapp.com', 'http://127.0.0.1/']
 
 
 # Application definition
